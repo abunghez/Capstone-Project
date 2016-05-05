aee3ff842d579186e0f1468adea03c32f05177f9
abunghez <andrei.bunghez@gmail.com>
2016-05-05 21:28:22 +0300

    Making statistics center static. Moving Area outside of StatisticsCenter

---
+++ app/src/main/java/com/compiler_error/flotto/data/Area.java
@@ -0,0 +1,45 @@
+package com.compiler_error.flotto.data;
+
+import android.location.Location;
+
+import java.util.ArrayList;
+
+/**
+ * Created by andrei on 05.05.2016.
+ */
+class Area {
+    Location center;
+    int totalSum;
+    ArrayList<Location> locations;
+    double sumLati, sumLongi;
+
+    public Area(Location c, int s) {
+        center = c;
+        totalSum = s;
+        locations  = new ArrayList<Location>();
+        locations.add(c);
+        sumLati = c.getLatitude();
+        sumLongi = c.getLongitude();
+    }
+
+    public void insert(Location l, int s) {
+        totalSum+=s;
+
+        sumLati += l.getLatitude();
+        sumLongi += l.getLongitude();
+
+        locations.add(l);
+
+        center.setLatitude(sumLati / locations.size());
+        center.setLongitude(sumLongi / locations.size());
+
+    }
+
+    public int getSum() {
+        return totalSum;
+    }
+
+    public Location getLocation() {
+        return center;
+    }
+}
